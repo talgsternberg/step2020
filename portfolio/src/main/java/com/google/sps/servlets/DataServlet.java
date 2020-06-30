d71b9e4445691d5d272a71ba839d3b9daaac1565
Tal <tal.g.sternberg.23@dartmouth.edu>
2020-06-30 13:08:25 +0000

    In DataServlet.java: add arrayList, convert to Jscon using Gson, import Gson. In pom.xml: set Gson dependency. In script.js: make json string from DataServlet.java appear on click of button

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,18 +15,27 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-
+  ArrayList<String> greetings=new ArrayList<String>();//Creating arraylist 
+  public DataServlet(){
+    greetings.add("hello");
+    greetings.add("whats up?");
+    greetings.add("nice to see you!");
+    }
+    
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello, Tal!</h1>");
+    String greetingJson = new Gson().toJson(greetings);
+    response.getWriter().println(greetingJson);
   }
 }
